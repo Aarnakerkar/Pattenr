0998c2eda4e0d70f49f3aef01640a8825b84628c
Aarnakerkar <rutujakadrekar@gmail.com>
2026-02-14 10:50:31 +0400

    parttern

--- rightangle.py
+++ rightangle.py
@@ -1,6 +1,10 @@
-print("half pyramid pattern")
+print("mirrored rightangle trianglr pattern")
 n=int(input("Enter the number of rows :"))
 for i in range(n):
     for j in range(i+1):
+        print("* ",end="")
+    print()
+for i in range(n):
+    for j in range(i-1):
         print("* ",end="")
     print()
